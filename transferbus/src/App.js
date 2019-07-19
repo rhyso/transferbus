d9b91a77ae74aa6872e33c0ba52ac80f4d99d1ed
rhyso <rhyswork@gmail.com>
2019-07-19 15:32:45 +0100

    initial setup

--- transferbus/src/App.js
+++ transferbus/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Book from './pages/Book'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <Router>
+        <div className="App">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about/">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/book/">Book</Link>
+                    </li>
+                </ul>
+            </nav>
 
-export default App;
+            <Route path="/" exact component={Home} />
+            <Route path="/about/" component={About} />
+            <Route path="/book/" component={Book} />
+        </div>
+    </Router>
+)
+
+export default App
+
---
+++ transferbus/src/pages/About.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+export default function About() {
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <h1>About</h1>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+        </div>
+    );
+}
